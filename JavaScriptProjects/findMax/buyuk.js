f60efd11f57a91525f3b064a5660f6f45f3769d7
senayakagunduz <senayakgndz@gmail.com>
2022-09-27 09:39:26 +0300

    css changed

--- JavaScriptProjects/findMax/buyuk.js
+++ JavaScriptProjects/findMax/buyuk.js
@@ -1,20 +1,31 @@
-const lblS = document.querySelector("#lblS");
-const calculateBtn = document.querySelector(".btn");
-const input1 = document.querySelector(".input1");
-const input2 = document.querySelector(".input2");
-const form = document.querySelector("#myForm");
-eventListeners();
+const formDom = document.querySelector("#myForm");
+formDom.addEventListener("submit", calculateMax);
 
-function eventListeners() {
-  form.addEventListener("submit", calculateMax);
-}
 function calculateMax(e) {
-  const x = input1.value.trim();
-  const y = input2.value.trim();
-  if (x > y) {
-    return (lblS.innerHTML = `${x} büyüktür`);
-  } else {
-    return (lblS.innerHTML = `${y} büyüktür`);
-  }
   e.preventDefault();
+  const firstInput = document.querySelector("#input1");
+  const secondInput = document.querySelector("#input2");
+
+    if(firstInput.value && secondInput.value){
+        if (firstInput.value > secondInput.value) { 
+           addItems(firstInput.value);  
+        } 
+        else if(secondInput.value>firstInput.value){ 
+           addItems(secondInput.value);
+        }else{
+          addItems(`${firstInput.value} and ${secondInput.value} are equals`)
+        }
+    }else{
+      alert("unvalid values");
+    }
+}
+
+let userListDom=document.querySelector("#userList");
+const addItems = (x,y)=>{
+  let liDom=document.createElement('li');
+  liDom.innerHTML=`max number is ${x},${y}`;
+  liDom.classList.add("list-group-item","mt-3");
+  userListDom.append(liDom);
+  
+
 }
