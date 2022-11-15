9af267c77a8a7d5ec4f9fbe5dc136d28ab723061
senayakagunduz <senayakgndz@gmail.com>
2022-11-15 14:01:24 +0300

    with post

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import getData from "./JavaScriptProjects/app.js";
+await getData(1);
 import hello,{topla,users,user} from "./my-module.js";
 console.log(topla(2,4));
 hello("mehmet");
 console.log(users);
 console.log(user);
+getData(1);
 
